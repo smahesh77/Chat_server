c726bf97956e16eddba3a32c364bdf91b175f3b6
smahesh77 <smahesh7736@gmail.com>
2023-05-13 10:07:33 +0530

    added functions to acces user data and messages

---
+++ public/js/main.js
@@ -0,0 +1,46 @@
+const socket = io()
+const chatmsgs = document.querySelector('.chat-messages')
+const chatForm = document.getElementById('chat-form')
+
+//get username and room from url
+const {username, room} = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+})
+
+console.log(username, room)
+
+//message from server
+socket.on('msg', message => {
+    //console.log("got it")
+    console.log(message)
+    outputmsg(message);
+
+    //scroll down
+    chatmsgs.scrollTop = chatmsgs.scrollHeight
+
+})
+
+chatForm.addEventListener('submit', e => {
+    e.preventDefault();// to stop it from defaultly saving the form
+    //getting message text
+    const msg = e.target.elements.msg.value// to get vaue from that textfield
+    
+    //sending the message back to server
+    socket.emit('chatMsg',msg)
+
+    //clear input
+    e.target.elements.msg.value =""
+    e.target.elements.msg.focus()
+
+})
+
+//output message to DOM
+function outputmsg(message){
+    const div = document.createElement('div')
+    div.classList.add('message')
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+    <p class="text">
+        ${message.text}
+    </p>`
+    document.querySelector('.chat-messages').appendChild(div)
+}
